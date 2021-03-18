311b39e2d05281626128808cfe493bf9dd8565d8
Nityan Khanna <nityan.khanna@gmail.com>
2021-03-18 14:39:30 -0400

    added documentation to the location class

--- src/main/java/tz/go/moh/him/thscp/mediator/wadau/portal/domain/Location.java
+++ src/main/java/tz/go/moh/him/thscp/mediator/wadau/portal/domain/Location.java
@@ -45,26 +45,56 @@ public class Location {
         this.setLongitude(longitude);
     }
 
+    /**
+     * Gets the district.
+     *
+     * @return Returns the district.
+     */
     public String getDistrict() {
         return district;
     }
 
+    /**
+     * Sets the district.
+     *
+     * @param district The district to set.
+     */
     public void setDistrict(String district) {
         this.district = district;
     }
 
+    /**
+     * Gets the latitude.
+     *
+     * @return Returns the latitude.
+     */
     public double getLatitude() {
         return latitude;
     }
 
+    /**
+     * Sets the latitude.
+     *
+     * @param latitude The latitude to set.
+     */
     public void setLatitude(double latitude) {
         this.latitude = latitude;
     }
 
+    /**
+     * Gets the longitude.
+     *
+     * @return Returns the longitude.
+     */
     public double getLongitude() {
         return longitude;
     }
 
+    /**
+     * Sets the longitude.
+     *
+     * @param longitude The longitude to set.
+     */
     public void setLongitude(double longitude) {
         this.longitude = longitude;
     }
